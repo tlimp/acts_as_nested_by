7403c6ab16279a27e80fb1580ba8f83d0b60e43c
Thomas Limp <thomas.limp@valiton.com>
2010-05-06 10:15:05 +0200

    better rdoc

--- lib/acts_as_nested_by.rb
+++ lib/acts_as_nested_by.rb
@@ -1,17 +1,28 @@
-module ActsAsNestedBy#:nodoc
-  def self.included(base)
+module ActsAsNestedBy #:nodoc:
+  def self.included(base) #:nodoc:
     base.send :extend, ClassMethods
   end
   
-  module ClassMethods #:nodoc
-    # Generates instance methods for setting and reading a nested_by_other flag
-    # raises ArgumentError if association to other does not exist or is not created by the :belongs_to macro
+  module ClassMethods
+    # Generates instance methods for setting and getting a 'nested_by_other' flag for each ':other' in association_names
+    # * Example:
+    #     Bar < ActiveRecord::Base
+    #       belongs_to :other
+    #       acts_as_nested_by :other
+    #     end
+    #
+    #   generates methods 
+    #     Bar#nested_by_other=
+    #     Bar#nested_by_other
+    #     Bar#nested_by_other?
+    # * Exceptions raised
+    #   * ArgumentError if one of the associations does not exist
+    #   * ArgumentError if one of the associations is not created by the :belongs_to macro
     def acts_as_nested_by(*association_names)
       association_names.each do |association_name|
         if reflection = reflect_on_association(association_name)
           if :belongs_to == reflection.macro
             class_eval %{
-            
               def nested_by_#{association_name}=(nested_by_#{association_name})
                 @nested_by_#{association_name} = nested_by_#{association_name}
               end
